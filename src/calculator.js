7c9787ea9f9fb4cc874ccab29fc249d99120989e
luke <luke@scribd.com>
2018-10-29 16:20:37 -0700

    forgot to add the calculator file with teh last commit

---
+++ src/calculator.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>calculate me</div>;
+  }
+}
