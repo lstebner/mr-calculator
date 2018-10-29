9456f3f8dc3238418253a7aca3f13a84163fb54a
luke <luke@scribd.com>
2018-10-29 16:20:07 -0700

    import Calculator and render it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import Calculator from "./calculator";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="calculator-main" />;
+    return (
+      <div className="calculator-main">
+        <Calculator />
+      </div>
+    );
   }
 }
 
