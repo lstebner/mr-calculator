58ea58d4708f121eb2837a2b4db45c1c765e4035
luke <luke@scribd.com>
2018-10-29 16:37:56 -0700

    adding click events to buttons

--- src/calculator.js
+++ src/calculator.js
@@ -3,8 +3,11 @@ import React from "react";
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      screen_value: 0
+      screen_value: 0,
+      number_a: 0,
+      number_b: 0
     };
   }
 
@@ -14,21 +17,91 @@ export default class Calculator extends React.Component {
         <span className="screen">{this.state.screen_value}</span>
         <hr />
         <div className="buttons">
-          <div className="button">0</div>
-          <div className="button">1</div>
-          <div className="button">2</div>
-          <div className="button">3</div>
-          <div className="button">4</div>
-          <div className="button">5</div>
-          <div className="button">6</div>
-          <div className="button">7</div>
-          <div className="button">8</div>
-          <div className="button">9</div>
-          <div className="button">+</div>
-          <div className="button">-</div>
-          <div className="button">=</div>
+          <div className="button" onClick={this.click_0}>
+            0
+          </div>
+          <div className="button" onClick={this.click_1}>
+            1
+          </div>
+          <div className="button" onClick={this.click_2}>
+            2
+          </div>
+          <div className="button" onClick={this.click_3}>
+            3
+          </div>
+          <div className="button" onClick={this.click_4}>
+            4
+          </div>
+          <div className="button" onClick={this.click_5}>
+            5
+          </div>
+          <div className="button" onClick={this.click_6}>
+            6
+          </div>
+          <div className="button" onClick={this.click_7}>
+            7
+          </div>
+          <div className="button" onClick={this.click_8}>
+            8
+          </div>
+          <div className="button" onClick={this.click_9}>
+            9
+          </div>
+          <div className="button" onClick={this.click_plus}>
+            +
+          </div>
+          <div className="button" onClick={this.click_minus}>
+            -
+          </div>
+          <div className="button" onClick={this.click_equals}>
+            =
+          </div>
         </div>
       </div>
     );
+  }
+
+  click_0() {
+    console.log("clicked 0");
+  }
+
+  click_1() {
+    console.log("clicked 1");
+  }
+  click_2() {
+    console.log("clicked 2");
+  }
+  click_3() {
+    console.log("clicked 3");
+  }
+  click_4() {
+    console.log("clicked 4");
+  }
+  click_5() {
+    console.log("clicked 5");
+  }
+  click_6() {
+    console.log("clicked 6");
+  }
+  click_7() {
+    console.log("clicked 7");
+  }
+  click_8() {
+    console.log("clicked 8");
+  }
+  click_9() {
+    console.log("clicked 9");
+  }
+
+  click_plus() {
+    console.log("clicked plus");
+  }
+
+  click_minus() {
+    console.log("clicked minus");
+  }
+
+  click_equals() {
+    console.loog("clicked equals");
   }
 }
