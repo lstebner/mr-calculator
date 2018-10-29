5abdc95752d2f5b3cf9ecfbbe56bdc68da247fd9
luke <luke@scribd.com>
2018-10-29 16:47:01 -0700

    setting some stuff ons tate when buttons are clicked

--- src/calculator.js
+++ src/calculator.js
@@ -7,7 +7,8 @@ export default class Calculator extends React.Component {
     this.state = {
       screen_value: 0,
       number_a: 0,
-      number_b: 0
+      number_b: 0,
+      operator: null
     };
   }
 
@@ -68,46 +69,102 @@ export default class Calculator extends React.Component {
   }
 
   click_0() {
+    if (this.state.number_a) {
+      this.setState({ number_a: 0 });
+    } else {
+      this.setState({ number_b: 0 });
+    }
+
     console.log("clicked 0");
   }
 
   click_1() {
+    if (this.state.number_a) {
+      this.setState({ number_a: 1 });
+    } else {
+      this.setState({ number_b: 1 });
+    }
     console.log("clicked 1");
   }
   click_2() {
+    if (this.state.number_a) {
+      this.setState({ number_a: 2 });
+    } else {
+      this.setState({ number_b: 2 });
+    }
     console.log("clicked 2");
   }
   click_3() {
+    if (this.state.number_a) {
+      this.setState({ number_a: 3 });
+    } else {
+      this.setState({ number_b: 3 });
+    }
     console.log("clicked 3");
   }
   click_4() {
+    if (this.state.number_a) {
+      this.setState({ number_a: 4 });
+    } else {
+      this.setState({ number_b: 4 });
+    }
     console.log("clicked 4");
   }
   click_5() {
+    if (this.state.number_a) {
+      this.setState({ number_a: 5 });
+    } else {
+      this.setState({ number_b: 5 });
+    }
     console.log("clicked 5");
   }
   click_6() {
+    if (this.state.number_a) {
+      this.setState({ number_a: 6 });
+    } else {
+      this.setState({ number_b: 6 });
+    }
     console.log("clicked 6");
   }
   click_7() {
+    if (this.state.number_a) {
+      this.setState({ number_a: 7 });
+    } else {
+      this.setState({ number_b: 7 });
+    }
     console.log("clicked 7");
   }
   click_8() {
+    if (this.state.number_a) {
+      this.setState({ number_a: 8 });
+    } else {
+      this.setState({ number_b: 8 });
+    }
     console.log("clicked 8");
   }
   click_9() {
+    if (this.state.number_a) {
+      this.setState({ number_a: 9 });
+    } else {
+      this.setState({ number_b: 9 });
+    }
     console.log("clicked 9");
   }
 
   click_plus() {
+    this.setState({ operator: "plus" });
     console.log("clicked plus");
   }
 
   click_minus() {
+    this.setState({ operator: "minus" });
     console.log("clicked minus");
   }
 
   click_equals() {
+    this.calculate();
     console.loog("clicked equals");
   }
+
+  calculate() {}
 }
